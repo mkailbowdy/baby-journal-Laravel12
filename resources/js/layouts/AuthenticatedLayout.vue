0da77a0d7da14ae4ea34d1bbcb5edd695e548d48
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2025-02-28 12:48:29 +0900

    Add navbar

--- resources/js/layouts/AuthenticatedLayout.vue
+++ resources/js/layouts/AuthenticatedLayout.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="min-h-screen bg-gray-100">
+        <NavBar />
         <!-- Page Heading -->
         <header class="bg-white shadow" v-if="$slots.header">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
@@ -13,3 +14,6 @@
         </main>
     </div>
 </template>
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue';
+</script>
